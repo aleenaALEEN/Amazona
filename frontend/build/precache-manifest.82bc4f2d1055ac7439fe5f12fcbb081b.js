3f7c421010e7aa0c34b91a6cb7140544f2a81d42
aleenafaisal <zxc312181@gmail.com>
2023-04-27 11:38:08 +0500

    amazona

---
+++ frontend/build/precache-manifest.82bc4f2d1055ac7439fe5f12fcbb081b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d5d9cbe992e954283e53876c09ce73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42c9de65a91523ca4006",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42c9de65a91523ca4006",
+    "url": "/static/js/main.7cbd61ce.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
